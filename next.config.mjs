3422d2a0062e64ba50294171194c284aed70c218
linuxbyter <makau1.peter@gmail.com>
2026-06-18 12:53:58 +0000

    fix: add env vars to next.config.mjs to ensure availability during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+};
 export default nextConfig;
